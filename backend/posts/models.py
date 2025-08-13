ec8feb0b58669b5f0c3275911ccf9e42734d0180
Alisher <alisherxujanov163@gmail.com>
2025-08-13 19:33:59 +0500

    Add Posts model with author relationship to user

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # CASCADE  => delete post if the user is deleted
+    # PREVENT  => prevent post from being deleted if the user is deleted
+    # SET_NULL => set the post to null if the user is deleted
